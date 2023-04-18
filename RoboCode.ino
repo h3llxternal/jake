2fc465df47bd45c6d88f56967599fc3522bf8cf7
RobertG22 <131201564+RobertG22@users.noreply.github.com>
2023-04-18 14:50:00 -0500

    Update RoboCode.ino

--- RoboCode.ino
+++ RoboCode.ino
@@ -1,1 +1,74 @@
+#define ENA 9
+#define IN1 8
+#define IN2 7
+#define ENB 10
+#define IN3 6
+#define IN4 5
+
+int trig = 2;
+int echo = 3;
+long duration;
+int distance;
+
+void setup() {
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(trig, LOW);
+    duration = pulseIn(echo, HIGH);
+    distance = duration * 0.034/2;
+    Serial.print("Distance");
+    Serial.print(distance);
+    
+    void Forward
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    delay(1000);
+    
+  void Left
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    delay(1000);
+    
+  void Back
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    delay(1000);
+    
+  void Stop
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    delay(1000);
+    
+    
+    
+}
 
